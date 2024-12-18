b1f2cc8b4145f03b673c2282786227bbf4c5b01b
Javier Seguel <javier.seguel.e@gmail.com>
2024-12-18 14:55:13 -0300

    comentarios y mostrar por pantalla

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,17 @@
+//esto es un comentario
+console.log("hola world!!!")
+console.log("hola world!!!")
+console.log("hola world!!!")
+
+/*
+console.log("hola world!!!")
+console.log("hola world!!!")
+console.log("hola world!!!")
+*/
+
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
